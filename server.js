b94942e7651e4c7b7d1ac7a7e868bf8009dc0f5e
Shruthi-Hebbar <30732637+Shruthi-Hebbar@users.noreply.github.com>
2017-08-07 00:10:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('article one opened and served here');
+  res.send('article one opened and served here');
 });
 
 app.get('/Article-two', function (req, res) {
-  res.sendFile('My heart goes shala la la laaaaa......');
+  res.send('My heart goes shala la la laaaaa......');
 });
 
 app.get('/Article-three', function (req, res) {
-  res.sendFile('savi save nanapu saavira nenapuuuuu');
+  res.send('savi save nanapu saavira nenapuuuuu');
 });
 
 app.get('/ui/style.css', function (req, res) {
