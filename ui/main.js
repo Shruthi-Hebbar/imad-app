5a2d2dee1c01d2d84cfc61b524e7179077823684
Shruthi-Hebbar <30732637+Shruthi-Hebbar@users.noreply.github.com>
2017-09-08 23:32:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+
+//Change the value of text on the webapp.
+var element = document.getElementById('main-text');
+
+element.innerHTML = "New value added through IMAD code console!";
