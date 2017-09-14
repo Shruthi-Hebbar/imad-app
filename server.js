a5a407fdeea25c4c9e67c684afe1011d7c90b9da
Shruthi-Hebbar <30732637+Shruthi-Hebbar@users.noreply.github.com>
2017-09-14 23:49:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+var config = {
+    user: 'aeroshru8931',
+    database: 'aeroshru8931',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/test-db', function (req, res) {
+  //make a select request
+  //provide the response
 });
 
 var counter = 0;
