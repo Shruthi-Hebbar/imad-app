c2e28cf6fafc2f7a38508c67aa63dd0507023ddb
Shruthi-Hebbar <30732637+Shruthi-Hebbar@users.noreply.github.com>
2017-09-18 23:39:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate(data) {}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -41,16 +43,22 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/Article-two', function (req, res) {
-  res.send('My heart goes shala la la laaaaa......');
-});
-
-app.get('/Article-three', function (req, res) {
-  res.send('savi save nanapu saavira nenapuuuuu');
+app.get('/articles/:articleName', function (req, res) {
+   var articleName = req.params.articleName;
+   
+   pool.query("SELECT * FROM article where title = '" + req.params.articleName +"'", function (err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           if (result.rows.length === 0) {
+               res.status(404).send('Article not found!!');
+           } else {
+               var articleData = result.rows [0];
+               res.send(createTemplate(articleData));
+           }
+       }
+       
+   });
 });
 
 app.get('/ui/style.css', function (req, res) {
