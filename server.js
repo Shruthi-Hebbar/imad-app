115fb92af57f929b8e5d78f145cae2ff5cdb3f66
Shruthi-Hebbar <30732637+Shruthi-Hebbar@users.noreply.github.com>
2017-09-21 00:03:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var Pool = require('pg').Pool;
 var config = {
@@ -15,6 +16,7 @@ var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.JSON());
 
 //function createTemplate(data) {}
 
@@ -33,8 +35,20 @@ app.get('/hash/:input', function (req, res) {
     res.send(hashedString);
 });
 
-app.get('/create-user', function (req, res) {
+app.post('/create-user', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
     
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, res) {
+      if(err) {
+          res.status(500).send(err.toString());
+      } else {
+          
+          res.send('User created succesfully!' + username);
+      }
+    });
 });
 
 
