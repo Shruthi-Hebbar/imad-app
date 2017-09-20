8bd599dbdb43aad0f4b0436523ec70cddef9fc97
Shruthi-Hebbar <30732637+Shruthi-Hebbar@users.noreply.github.com>
2017-09-20 23:46:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.get('/hash/:input', function (req, res) {
     res.send(hashedString);
 });
 
+app.get('/create-user', function (req, res) {
+    
+});
+
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
