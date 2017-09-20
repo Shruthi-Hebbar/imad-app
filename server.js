13476e4de367fd6a0fde4538e4ddb7881075ed75
Shruthi-Hebbar <30732637+Shruthi-Hebbar@users.noreply.github.com>
2017-09-20 23:34:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,14 +11,28 @@ var config = {
     password: process.env.DB_PASSWORD
 };
 
+var crypto = require('crypto');
+
 var app = express();
 app.use(morgan('combined'));
 
-function createTemplate(data) {}
+//function createTemplate(data) {}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash (input, salt) {
+    var hashed = crypto.pbkdf2sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function (req, res) {
+    var hashedString = hash(req.params.input, 'this-is-a-random-string'); 
+    res.send(hashedString);
+});
+
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
